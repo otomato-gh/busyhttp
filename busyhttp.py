0c7fa766b5ebff83bfaf96995cd5089ca97049a2
Ant Weiss <ant.weiss@gmail.com>
2022-05-25 16:48:07 +0300

    Add memory handler

--- busyhttp.py
+++ busyhttp.py
@@ -1,12 +1,24 @@
 import time
 
+MEGABYTE = 1024 * 1024
+
 from flask import Flask
 application = Flask(__name__)
 
-@application.route("/")
-def busy():
+class memBuf:
+    buffer = []
+    
+@application.route("/cpu")
+def busy_cpu():
     until = time.time() + 1
     while time.time() < until:
         pass
     return "I've been busy for 1 second.\n"
+
+@application.route("/memory")
+def busy_memory():
+    buffer += ['A' * MEGABYTE for _ in range(0, 1)]
+    time.sleep(1)
+    return "I've consumed 1 MB of memory.\n"
+
 
